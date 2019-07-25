093e98d61a554920b3eac7ad201e655d8e2dd81a
Jamie Vullo <jvullo@protonmail.com>
2019-07-25 17:56:18 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,7 +16,7 @@ class Genre < ActiveRecord::Base
     # return an array of strings containing every musician's name
     binding.pry 
     
-    self.artists.map do {|x|  
+    self.artists.map do {|x| x.name} 
     
   end
 end
