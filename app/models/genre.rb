64328962a942e04968d604e0100d091529f8757b
Jamie Vullo <jvullo@protonmail.com>
2019-07-25 17:57:34 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,9 +14,9 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry 
+    #binding.pry 
     
     self.artists.map {|x| x.name} 
-   end 
+    
   end
 end
